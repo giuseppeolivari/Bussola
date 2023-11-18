e09b26f757fd7a72085430228f556b02764abedf
Giuseppe <golivari1@gmail.com>
2023-11-18 10:55:10 +0100

    Initial Commit

---
+++ Bussola/BussolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BussolaApp.swift
+//  Bussola
+//
+//  Created by Giuseppe Olivari on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BussolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
