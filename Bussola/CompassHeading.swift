56b4ef7f72f0ebe476d8d09647cadd219cfa90ea
Giuseppe <golivari1@gmail.com>
2023-11-26 22:40:27 +0100

    1

---
+++ Bussola/CompassHeading.swift
@@ -0,0 +1,43 @@
+//
+//  CompassHeading.swift
+//  Bussola
+//
+//  Created by Giuseppe Olivari on 26/11/23.
+//
+
+
+import Foundation
+import Combine
+import CoreLocation
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var gradi: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.gradi = -1 * newHeading.magneticHeading
+    }
+}
